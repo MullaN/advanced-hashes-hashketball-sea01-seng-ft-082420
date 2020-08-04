346a91dc996816d2528bbad8b55fb28ef8600f51
Andrew Mullan <admullan@gmail.com>
2020-08-04 23:25:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,7 +199,7 @@ def winning_team
   game = game_hash
   final_score = {}
   game.each do |home_away, team_info|
-    final_score[team_info[:team_name]] = team_info[:players].sum {|player| player[:score]}
+    final_score.add(team_info[:team_name]] => team_info[:players].sum {|player| player[:score]})
     binding.pry
   end
 end
